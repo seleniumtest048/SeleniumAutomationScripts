3b7d1c96d93b4ac397a70725241a06a0d515a54d
Rishiarip <raripineni@gmail.com>
2014-07-18 18:33:56 +0530

    WHOis Project Automation Scripts

--- Framework/src/test/java/com/projectname/utils/ActionEventRequest.java
+++
@@ -1,9 +0,0 @@
-package com.projectname.utils;
-
-public class ActionEventRequest {
-
-	String keyword;
-	String keywordtype;
-	String object;
-	String objectProp;
-}
--- Framework/src/test/java/com/projectname/utils/ActionEventResult.java
+++
@@ -1,9 +0,0 @@
-package com.projectname.utils;
-
-import org.openqa.selenium.WebElement;
-
-public class ActionEventResult {
-
-	 public WebElement welement=null;
-	 public String result="res";
-}
